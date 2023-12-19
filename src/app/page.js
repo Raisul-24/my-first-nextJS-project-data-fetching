0cdf38a8ecbdeb3fd6473ed727a72f2e5b7fe290
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-12-19 23:47:25 +0600

    next.js project setup

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-center text-3xl font-medium text-orange-300'>Welcome to data fatching</h1>
+    </div>
+  );
+};
+
+export default HomePage;
